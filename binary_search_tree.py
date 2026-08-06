8479c4ddf821c67c2c3a9e4c6d1fd4f558b66531
ZAID BHARDE <zaidbharde09@gmail.com>
2026-08-06 23:47:33 +0530

    Create binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        self.root = self._insert(self.root, val)
+
+    def _insert(self, node, val):
+        if node is None:
+            return Node(val)
+        if val < node.val:
+            node.left = self._insert(node.left, val)
+        else:
+            node.right = self._insert(node.right, val)
+        return node
+
+    def inorder(self):
+        result = []
+        self._inorder(self.root, result)
+        return result
+
+    def _inorder(self, node, result):
+        if node:
+            self._inorder(node.left, result)
+            result.append(node.val)
+            self._inorder(node.right, result)
+
+    def search(self, val):
+        node = self.root
+        while node:
+            if val == node.val:
+                return True
+            node = node.left if val < node.val else node.right
+        return False
+
+
+if __name__ == "__main__":
+    bst = BST()
+    for v in [50, 30, 70, 20, 40, 60, 80]:
+        bst.insert(v)
+    print("Inorder:", bst.inorder())
+    print("Search 40:", bst.search(40))
+    print("Search 45:", bst.search(45))
